f7064b4f53b0b5ab1366bb537e8935d9ae71f4d8
Lic. Manuel Aguado Martinez <maguado@cenatav.co.cu>
2017-08-16 11:54:09 -0400

    Adding the two peaks global threshold method

---
+++ global_th/two_peaks.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import numpy as np
+from scipy.ndimage import gaussian_filter
+
+__copyright__ = 'Copyright 2017'
+__author__ = u'Lic. Manuel Aguado Martínez'
+
+
+def two_peaks_threshold(image, smooth_hist=True, sigma=5):
+    """Runs the two peaks threshold algorithm. It selects two peaks
+    from the histogram and return the index of the minimum value
+    between them.
+    
+    The first peak is deemed to be the maximum value fo the histogram,
+    while the algorithm will look for the second peak by multiplying the
+    histogram values by the square of the distance from the first peak.
+    This gives preference to peaks that are not close to the maximum.
+
+    @param image: The input image
+    @type image: ndarray
+    @param smooth_hist: Indicates whether to smooth the input image
+        histogram before finding peaks.
+    @type smooth_hist: bool
+    @param sigma: The sigma value for the gaussian function used to
+        smooth the histogram.
+    @type sigma: int
+    
+    @return: The threshold between the two founded peaks with the
+        minimum histogram value
+    @rtype: int
+    """
+    hist = np.histogram(image, bins=256)[0]
+
+    if smooth_hist:
+        hist = gaussian_filter(hist, sigma=sigma)
+
+    f_peak = np.argmax(hist)
+
+    # finding second peak
+    s_peak = np.argmax((np.arange(len(hist)) - f_peak) ** 2 * hist)
+
+    thr = np.argmin(hist[min(f_peak, s_peak): max(f_peak, s_peak)])
+    thr += min(f_peak, s_peak)
+
+    return thr
