4ef539c34644a86c6881f4461f091041b29dbea7
Lic. Manuel Aguado Martinez <maguado@cenatav.co.cu>
2017-08-16 15:06:39 -0400

    [min_err] Refactoring code

--- global_th/min_err.py
+++ global_th/min_err.py
@@ -21,35 +21,40 @@ def min_err_threshold(image):
     @return: The threshold that minimize the error
     @rtype: int
     """
-
+    # Input image histogram
     hist = np.histogram(image, bins=256)[0]
-    c_hist = hist.cumsum()
-    inv_c_histogram = c_hist[len(c_hist) - 1] - c_hist
 
-    # TODO Improve the efficiency of the next calculus by using vectorize
-    weight_cdf = (hist * np.arange(len(hist))).cumsum()
-    black_mean = weight_cdf * (c_hist != 0) / (c_hist + (c_hist == 0))
-    white_mean = (weight_cdf[len(weight_cdf) - 1] - weight_cdf) *\
-                 (inv_c_histogram != 0) / (inv_c_histogram + (inv_c_histogram == 0))
+    # The number of background pixels for each threshold
+    w_backg = hist.cumsum()
+
+    # The number of foreground pixels for each threshold
+    w_foreg = w_backg[-1] - w_backg
+
+    # Cumulative distribution function
+    cdf = np.cumsum(hist * np.arange(len(hist)))
+
+    # Means
+    b_mean = cdf * (w_backg != 0) / (w_backg + (w_backg == 0))
+    w_mean = (cdf[-1] - cdf) * (w_foreg != 0) / (w_foreg + (w_foreg == 0))
 
     threshold = 0
     min_error = -1
 
     for t in xrange(1, len(hist)):
-        if c_hist[t] != 0 and inv_c_histogram[t] != 0:
+        if w_backg[t] != 0 and w_foreg[t] != 0:
             # TODO Compute these values outside the for loop
-            black_std = (np.arange(t+1) - black_mean[t])**2 * hist[:t+1]
-            black_std = black_std.sum() / c_hist[t]
-            white_std = ((np.arange(t+1, len(hist)) - white_mean[t])**2 *
+            black_std = (np.arange(t+1) - b_mean[t])**2 * hist[:t+1]
+            black_std = black_std.sum() / w_backg[t]
+            white_std = ((np.arange(t+1, len(hist)) - w_mean[t])**2 *
                          hist[t+1:len(hist)])
-            white_std = white_std.sum() / inv_c_histogram[t]
+            white_std = white_std.sum() / w_foreg[t]
 
             if white_std != 0 and black_std != 0:
-                error = 1 + 2 * (c_hist[t] * np.log(black_std) + inv_c_histogram[t] *
-                                 np.log(white_std)) - 2 * (c_hist[t] *
-                                                           np.log(c_hist[t])
-                                                           + inv_c_histogram[t] *
-                                                           np.log(inv_c_histogram[t]))
+                error = 1 + 2 * (w_backg[t] * np.log(black_std) + w_foreg[t] *
+                                 np.log(white_std)) - 2 * (w_backg[t] *
+                                                           np.log(w_backg[t])
+                                                           + w_foreg[t] *
+                                                           np.log(w_foreg[t]))
                 if min_error == -1 or min_error > error:
                     min_error = error
                     threshold = t
