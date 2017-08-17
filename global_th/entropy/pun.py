4a150082f897e50ef570ab80b3eaef9f2028b30d
Lic. Manuel Aguado Martinez <maguado@cenatav.co.cu>
2017-08-17 13:08:15 -0400

    [pun] Refactoring code

--- global_th/entropy/pun.py
+++ global_th/entropy/pun.py
@@ -21,8 +21,9 @@ def pun_threshold(image):
     @rtype: int
     """
     histogram = np.histogram(image, bins=256, normed=True)[0]
-    entropy_cdf = np.cumsum(histogram * np.log(histogram + (histogram == 0)))
     cdf = np.cumsum(histogram)
+    entropy_cdf = np.cumsum(histogram * np.log(histogram + (histogram == 0)))
+    entropy_ratios = entropy_cdf / entropy_cdf[-1]
 
     max_entropy = 0
     threshold = 0
@@ -31,9 +32,9 @@ def pun_threshold(image):
         black_max = np.max(histogram[:t + 1])
         white_max = np.max(histogram[t + 1:])
         if black_max * white_max > 0:
-            entropy_ratio = entropy_cdf[t] / entropy_cdf[len(histogram) - 1]
-            x = entropy_ratio * np.log(cdf[t])/np.log(black_max)
-            y = 1.0 - entropy_ratio
+            e_ratio = entropy_ratios[t]
+            x = e_ratio * np.log(cdf[t])/np.log(black_max)
+            y = 1.0 - e_ratio
             z = np.log(1 - cdf[t]) / np.log(white_max)
             entropy = x + y * z
 
