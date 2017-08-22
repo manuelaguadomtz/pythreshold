37ea8373b6acf33c60671239bd0cb0c2b8600707
Lic. Manuel Aguado Martinez <manuelaguadomtz@gmail.com>
2017-08-21 20:00:56 -0400

    [kapur] Refactoring code

--- global_th/entropy/kapur.py
+++ global_th/entropy/kapur.py
@@ -7,34 +7,6 @@ __copyright__ = 'Copyright 2017'
 __author__ = u'Lic. Manuel Aguado Martínez'
 
 
-def __compute_entropy(probability, cum_probability):
-    """"Compute the entropy of a gray value given his probability
-    and cumulative probability
-    """
-    return 0.0 if probability <= 0 or cum_probability <= 0 else \
-        -probability / cum_probability * np.log(probability / cum_probability)
-
-
-def __compute_total_entropy(histogram, cdf, threshold):
-    """Compute the total entropy given a threshold
-
-    Keywords:
-    histogram -- The input image histogram
-    cdf -- The cumulative histogram function
-    threshold -- The threshold to distinguish between black and white pixels
-    """
-    white_entropy = 0
-    black_entropy = 0
-    for i in xrange(0, len(histogram)):
-        if i <= threshold:
-            black_entropy += __compute_entropy(histogram[i], cdf[threshold])
-        else:
-            white_entropy += __compute_entropy(histogram[i],
-                                               1.0 - cdf[threshold])
-
-    return black_entropy + white_entropy
-
-
 def kapur_threshold(image):
     """ Runs the Kapur's threshold algorithm.
 
@@ -50,15 +22,17 @@ def kapur_threshold(image):
     @rtype: int
     """
     hist = np.histogram(image, range=(0, 255), bins=255, density=True)[0]
-    cdf = hist.cumsum()
+    c_hist = hist.cumsum()
+    c_hist_i = 1.0 - c_hist
 
-    max_entropy = 0
-    threshold = 0
+    # To avoid invalid operations regarding 0.
+    c_hist[c_hist == 0] = 1
+    c_hist_i[c_hist_i == 0] = 1
 
-    for t in xrange(len(hist) - 1):
-        entropy = __compute_total_entropy(hist, cdf, t)
-        if max_entropy < entropy:
-            max_entropy = entropy
-            threshold = t
+    c_entropy = (hist * np.log(hist + (hist == 0))).cumsum()
+    b_entropy = -c_entropy / c_hist + np.log(c_hist)
 
-    return threshold
+    c_entropy_i = c_entropy[-1] - c_entropy
+    f_entropy = -c_entropy_i / c_hist_i + np.log(c_hist_i)
+
+    return np.argmax(b_entropy + f_entropy)
