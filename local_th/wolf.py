e95aaec4240465fb5d9d98cd060b444121d7de00
Lic. Manuel Aguado Martinez <maguado@cenatav.co.cu>
2017-08-24 11:27:36 -0400

    Adding Wolf threshold method

---
+++ local_th/wolf.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import numpy as np
+
+__copyright__ = 'Copyright 2017'
+__author__ = u'Lic. Manuel Aguado Martínez'
+
+
+def wolf_threshold(img, w_size=15, k=0.5):
+    """ Runs the Wolf's thresholding algorithm.
+
+    Reference:
+    C. Wolf, J-M. Jolion, “Extraction and Recognition
+    of Artificial Text in Multimedia Documents”, Pattern Analysis and
+    Applications, 6(4):309-326, (2003).
+
+    Modifications: Using integral images to compute the local mean and the
+    standard deviation
+
+    @param img: The input image
+    @type img: ndarray
+    @param w_size: The size of the local window to compute
+        each pixel threshold. Should be and odd value
+    @type w_size: int 
+    @param k: Controls the value of the local threshold.
+    @type k: float
+    
+    @return: The estimated local threshold for each pixel
+    @rtype: ndarray
+    """
+    # Obtaining rows and cols
+    rows, cols = img.shape
+    i_rows, i_cols = rows + 1, cols + 1
+
+    # Computing integral images
+    # Leaving first row and column in zero for convenience
+    integral = np.zeros((i_rows, i_cols), np.float)
+    sqr_integral = np.zeros((i_rows, i_cols), np.float)
+
+    integral[1:, 1:] = np.cumsum(np.cumsum(img, axis=1), axis=0)
+    sqr_img = np.square(img.astype(np.float))
+    sqr_integral[1:, 1:] = np.cumsum(np.cumsum(sqr_img, axis=1), axis=0)
+
+    # Defining grid
+    x, y = np.meshgrid(np.arange(1, i_rows), np.arange(1, i_cols))
+
+    # Obtaining local coordinates
+    hw_size = w_size / 2
+    x1 = (x - hw_size).clip(1, cols)
+    x2 = (x + hw_size).clip(1, cols)
+    y1 = (y - hw_size).clip(1, rows)
+    y2 = (y + hw_size).clip(1, rows)
+
+    # Obtaining local areas size
+    l_size = (x2 - x1 + 1) * (y2 - y1 + 1)
+
+    # Computing sums
+    sums = (integral[y2, x2] - integral[y2, x1 - 1] -
+            integral[y1 - 1, x2] + integral[y1 - 1, x1 - 1])
+    sqr_sums = (sqr_integral[y2, x2] - sqr_integral[y2, x1 - 1] -
+                sqr_integral[y1 - 1, x2] + sqr_integral[y1 - 1, x1 - 1])
+
+    # Computing local means
+    means = sums / l_size
+
+    # Computing local standard deviation
+    stds = np.sqrt(sqr_sums / l_size - np.square(means))
+
+    # Computing min and max values
+    max_std = np.max(stds)
+    min_v = np.min(img)
+
+    # Computing thresholds
+    thresholds = ((1.0 - k) * means + k * min_v + k * stds /
+                  max_std * (means - min_v))
+
+    return thresholds
