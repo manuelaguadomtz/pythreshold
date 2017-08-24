51e2183ccb2e7abbd3f720036cb6f9b024fba591
Lic. Manuel Aguado Martinez <maguado@cenatav.co.cu>
2017-08-24 11:03:27 -0400

    Adding local sauvola threshold method

---
+++ local_th/sauvola.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import numpy as np
+
+__copyright__ = 'Copyright 2014'
+__author__ = u'Lic. Manuel Aguado Martínez'
+
+
+def sauvola_threshold(img, w_size=15, k=0.35):
+    """ Runs the sauvola's thresholding algorithm.
+    
+    Reference:
+    Sauvola, J., Seppanen, T., Haapakoski, S., and Pietikainen, M.:
+    ‘Adaptive document thresholding’. Proc. 4th Int. Conf. on Document
+    Analysis and Recognition, Ulm Germany, 1997, pp. 147–152.
+
+    Modifications: Using integral images to compute local mean
+        and standard deviation
+
+    @param img: The input image
+    @type img: ndarray
+    @param w_size: The size of the local window to compute
+        each pixel threshold. Should be and odd value
+    @type w_size: int 
+    @param k: Controls the value of the local threshold. It lies in the
+        interval [0.2, 0.5]
+    @type k: float
+    
+    @return: The estimated local threshold for each pixel
+    @rtype: ndarray
+    """
+    # Obtaining rows and cols
+    rows, cols = img.shape
+    i_rows, i_cols = rows + 1, cols + 1
+
+    # Computing integral images
+    # Leaving first row and column in zero for convenience
+    integral = np.zeros((i_rows, i_cols), np.float)
+    sqr_integral = np.zeros((i_rows, i_cols), np.float)
+
+    integral[1:, 1:] = np.cumsum(np.cumsum(img, axis=1), axis=0)
+    sqr_img = np.square(img.astype(np.float))
+    sqr_integral[1:, 1:] = np.cumsum(np.cumsum(sqr_img, axis=1), axis=0)
+
+    # Defining grid
+    grid = np.meshgrid(np.arange(1, i_rows), np.arange(1, i_cols))
+    x = grid[0].ravel()
+    y = grid[1].ravel()
+
+    # Obtaining local coordinates
+    hw_size = w_size / 2
+    x1 = (x - hw_size).clip(1, cols)
+    x2 = (x + hw_size).clip(1, cols)
+    y1 = (y - hw_size).clip(1, rows)
+    y2 = (y + hw_size).clip(1, rows)
+
+    # Obtaining local areas size
+    l_size = (x2 - x1 + 1) * (y2 - y1 + 1)
+
+    # Computing sums
+    sums = (integral[y2, x2] - integral[y2, x1 - 1] -
+            integral[y1 - 1, x2] + integral[y1 - 1, x1 - 1])
+    sqr_sums = (sqr_integral[y2, x2] - sqr_integral[y2, x1 - 1] -
+                sqr_integral[y1 - 1, x2] + sqr_integral[y1 - 1, x1 - 1])
+
+    # Computing local means
+    means = sums / l_size
+
+    # Computing local standard deviation
+    stds = np.sqrt(sqr_sums / l_size - np.square(means))
+
+    # Computing thresholds
+    thresholds = means * (1.0 + k * (stds / 128 - 1.0))
+
+    return thresholds.reshape((rows, cols))
