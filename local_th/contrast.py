f2b949293b4c613c8cc0d7559fc2210a4f72e4f8
Lic. Manuel Aguado Martinez <maguado@cenatav.co.cu>
2017-08-25 13:56:16 -0400

    Adding local contrast thresholding method

--- local_th/contrast.py
+++ local_th/contrast.py
@@ -5,49 +5,44 @@ import numpy as np
 
 from skimage.util.shape import view_as_windows
 
-__copyright__ = 'Copyright 2017'
+__copyright__ = 'Copyright 2015'
 __author__ = u'Lic. Manuel Aguado Martínez'
 
 
-def bernsen_threshold(img, w_size=15, c_thr=30):
-    """Runs the Bernsen thresholding algorithm
-    
-    Reference:
-    Bernsen, J (1986), "Dynamic Thresholding of Grey-Level Images",
-    Proc. of the 8th Int. Conf. on Pattern Recognition
+def contrast_threshold(img, w_size=15):
+    """Runs the contrast thresholding algorithm
 
-    
+    Reference:
+
+
     @param img: The input image. Must be a gray scale image
     @type img: ndarray
     @param w_size: The size of the local window to compute
         each pixel threshold. Should be an odd window.
     @type w_size: int
-    @param c_thr: The threshold contrast to determine an
-        homogeneous region
-    @type c_thr: int
-    
+
     @return: The estimated local threshold for each pixel
     @rtype: ndarray
     """
-    thresholds = np.zeros(img.shape, np.uint8)
+    thresholds = np.zeros(img.shape)
 
     # Obtaining windows
     hw_size = w_size / 2
     padded_img = np.ones((img.shape[0] + w_size - 1,
                           img.shape[1] + w_size - 1)) * np.nan
     padded_img[hw_size: -hw_size,
-               hw_size: -hw_size] = img
+    hw_size: -hw_size] = img
 
     winds = view_as_windows(padded_img, (w_size, w_size))
 
+    # Obtaining maximums and minimums
     mins = np.nanmin(winds, axis=(2, 3))
     maxs = np.nanmax(winds, axis=(2, 3))
 
-    # Calculating contrast and mid values
-    contrast = maxs - mins
-    mid_vals = (maxs + mins) / 2
+    min_dif = img - mins
+    max_dif = maxs - img
 
-    thresholds[contrast <= c_thr] = 128
-    thresholds[contrast > c_thr] = mid_vals[contrast > c_thr]
+    thresholds[min_dif <= max_dif] = 256
+    thresholds[min_dif > max_dif] = 0
 
     return thresholds
