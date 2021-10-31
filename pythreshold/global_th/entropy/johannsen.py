Fixing histogram computation (number of bins)

pythreshold/global_th/entropy/johannsen.py
@@ -20,7 +20,7 @@ def johannsen_threshold(image):
     @return: The estimated threshold
     @rtype: int
     """
-    hist, _ = np.histogram(image, bins=range(256), density=True)
+    hist, _ = np.histogram(image, bins=range(257), density=True)
 
     # Knowing the number of leading zeros
     l_zeros = 0

pythreshold/global_th/entropy/kapur.py
@@ -23,7 +23,7 @@ def kapur_threshold(image):
     @return: The estimated threshold
     @rtype: int
     """
-    hist, _ = np.histogram(image, bins=range(256), density=True)
+    hist, _ = np.histogram(image, bins=range(257), density=True)
     c_hist = hist.cumsum()
     c_hist_i = 1.0 - c_hist
 
@@ -119,7 +119,7 @@ def kapur_multithreshold(image, nthrs):
     @rtype: int
     """
     # Histogran
-    hist, _ = np.histogram(image, bins=range(256), density=True)
+    hist, _ = np.histogram(image, bins=range(257), density=True)
 
     # Cumulative histogram
     c_hist = hist.cumsum()

pythreshold/global_th/entropy/pun.py
@@ -20,7 +20,7 @@ def pun_threshold(image):
     @rtype: int
     """
     # Calculating histogram
-    hist, _ = np.histogram(image, bins=range(256), density=True)
+    hist, _ = np.histogram(image, bins=range(257), density=True)
 
     # Calculating histogram cumulative sum
     hcs = np.cumsum(hist)

pythreshold/global_th/otsu.py
@@ -30,7 +30,7 @@ def otsu_threshold(image=None, hist=None):
 
     # Calculating histogram
     if not hist:
-        hist = np.histogram(image, bins=range(256))[0].astype(np.float)
+        hist = np.histogram(image, bins=range(257))[0].astype(np.float)
 
     cdf_backg = np.cumsum(np.arange(len(hist)) * hist)
     w_backg = np.cumsum(hist)  # The number of background pixels
@@ -137,7 +137,7 @@ def otsu_multithreshold(image=None, hist=None, nthrs=2):
 
     # Calculating histogram
     if not hist:
-        hist = np.histogram(image, bins=range(256))[0].astype(np.float)
+        hist = np.histogram(image, bins=range(257))[0].astype(np.float)
 
     # Cumulative histograms
     c_hist = np.cumsum(hist)

pythreshold/global_th/p_tile.py
@@ -23,7 +23,7 @@ def p_tile_threshold(image, pct):
     @rtype int
     """
     n_pixels = pct * image.shape[0] * image.shape[1]
-    hist = np.histogram(image, bins=range(256))[0]
+    hist = np.histogram(image, bins=range(257))[0]
     hist = np.cumsum(hist)
 
     return np.argmin(np.abs(hist - n_pixels))

pythreshold/global_th/two_peaks.py
@@ -34,7 +34,7 @@ def two_peaks_threshold(image, smooth_hist=True, sigma=5):
         minimum histogram value
     @rtype: int
     """
-    hist = np.histogram(image, bins=range(256))[0].astype(np.float)
+    hist = np.histogram(image, bins=range(257))[0].astype(np.float)
 
     if smooth_hist:
         hist = gaussian_filter(hist, sigma=sigma)